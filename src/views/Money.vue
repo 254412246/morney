cea8c56bdae9de53cd726bd96b75e431dd8e2f77
Administrator <254412246@qq.com>
2020-03-18 19:17:35 +0800

    Money页面布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,18 +1,30 @@
 <template>
-    <div>money
-    <Nav/>
+    <div class="nav-wrapper">
+        <div class="content">
+            <p>money</p>
+        </div>
+        <Nav/>
     </div>
 </template>
 
 <script>
 
 
-  export default {
-    name: "Money",
+    export default {
+        name: "Money",
 
-  }
+    }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+.nav-wrapper{
+    border: 1px solid green;
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+}
+    .content{
+        overflow: auto;
+        flex-grow: 1;
+    }
 </style>
