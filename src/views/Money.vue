b6037a71836ae3655c0a5d70721ae541d5fec5cf
Administrator <254412246@qq.com>
2020-03-29 01:33:01 +0800

    数据打包，添加时间

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,7 @@
 <template>
     <Layout class-prefix="layout">
-        {{record}}
-        <NumberPad @update:value="abc"/>
+        {{recordList}}
+        <NumberPad @update:value="abc" @submit="saveRecord"/>
         <Types :value.sync="record.type"/>
         <Notes @update:value="qqq"/>
         <Tags :data-source.sync="tags" @update:value="yyy"/>
@@ -10,17 +10,18 @@
 
 <script lang="ts">
     import Vue from 'vue';
-    import {Component} from 'vue-property-decorator';
+    import {Component, Watch} from 'vue-property-decorator';
     import NumberPad from '@/components/Money/NumberPad.vue';
     import Types from '@/components/Money/Types.vue';
     import Notes from '@/components/Money/Notes.vue';
     import Tags from '@/components/Money/Tags.vue';
-
+    const recordList: Record[] = JSON.parse(window.localStorage.getItem('recordList') || '[]');
     type Record = {
         tags: string[];
         notes: string;
         type: string;
         amount: number;
+        createdAt?: Date;
     }
     @Component({
         components: {Tags, Notes, Types, NumberPad}
@@ -28,9 +29,12 @@
 
     export default class Money extends Vue {
         tags = ['衣', '食', '住', '行', '彩票'];
+
+        recordList: Record[] = recordList;
         record: Record = {
             tags: [], notes: '', type: '-', amount: 0
         };
+
 
         yyy(value: string[]) {
             this.record.tags = value;
@@ -43,6 +47,16 @@
         abc(value: string) {
             this.record.amount = parseFloat(value);
         }
+        saveRecord(){
+            const record2: Record = JSON.parse(JSON.stringify(this.record));
+            record2.createdAt = new Date();
+            this.recordList.push(record2);
+        }
+        @Watch('recordList')
+        onRecordListChange() {
+            window.localStorage.setItem('recordList', JSON.stringify(this.recordList));
+        }
+
     }
 </script>
 
