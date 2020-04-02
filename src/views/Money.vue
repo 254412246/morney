4f75352517a635085d6695d158d7dac03efaf2cd
Administrator <254412246@qq.com>
2020-04-02 20:17:05 +0800

    展示和创建标签页

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,5 @@
 <template>
     <Layout class-prefix="layout">
-        {{recordList}}
         <NumberPad @update:value="abc" @submit="saveRecord"/>
         <Types :value.sync="record.type"/>
         <Notes @update:value="qqq"/>
@@ -10,28 +9,26 @@
 
 <script lang="ts">
     import Vue from 'vue';
-    import {Component, Watch} from 'vue-property-decorator';
     import NumberPad from '@/components/Money/NumberPad.vue';
     import Types from '@/components/Money/Types.vue';
     import Notes from '@/components/Money/Notes.vue';
     import Tags from '@/components/Money/Tags.vue';
-    const recordList: Record[] = JSON.parse(window.localStorage.getItem('recordList') || '[]');
-    type Record = {
-        tags: string[];
-        notes: string;
-        type: string;
-        amount: number;
-        createdAt?: Date;
-    }
+    import {Component, Watch} from 'vue-property-decorator';
+    import recordListModel from '@/models/recordListModel';
+    import tagListModel from '@/models/tagListModel';
+
+    const recordList = recordListModel.fetch();
+    const tagList = tagListModel.fetch();
+
     @Component({
         components: {Tags, Notes, Types, NumberPad}
     })
 
     export default class Money extends Vue {
-        tags = ['衣', '食', '住', '行', '彩票'];
+        tags = tagList;
 
-        recordList: Record[] = recordList;
-        record: Record = {
+        recordList: RecordItem[] = recordList;
+        record: RecordItem = {
             tags: [], notes: '', type: '-', amount: 0
         };
 
@@ -47,14 +44,16 @@
         abc(value: string) {
             this.record.amount = parseFloat(value);
         }
-        saveRecord(){
-            const record2: Record = JSON.parse(JSON.stringify(this.record));
-            record2.createdAt = new Date();
-            this.recordList.push(record2);
+
+        saveRecord() {
+            const record22: RecordItem = recordListModel.clone(this.record);
+            record22.createdAt = new Date();
+            this.recordList.push(record22);
         }
+
         @Watch('recordList')
         onRecordListChange() {
-            window.localStorage.setItem('recordList', JSON.stringify(this.recordList));
+            recordListModel.save(this.recordList);
         }
 
     }
