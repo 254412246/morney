ba2baa4a5f848f0970d161d0ccea2549204470d8
Administrator <254412246@qq.com>
2020-04-23 20:34:51 +0800

    完成 删除标签 css

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -2,9 +2,12 @@
     <Layout class-prefix="layout">
         <NumberPad @update:value="abc" @submit="saveRecord"/>
         <Types :value.sync="record.type"/>
-        <Notes field-name="备注"
-               placeholder="请输入备注"
-               @update:value="qqq"/>
+        <div class="notes">
+            <FormItem field-name="备注"
+                      placeholder="请输入备注"
+                      @update:value="qqq"/>
+        </div>
+
         <Tags :data-source.sync="tags" @update:value="yyy"/>
     </Layout>
 </template>
@@ -13,7 +16,7 @@
     import Vue from 'vue';
     import NumberPad from '@/components/Money/NumberPad.vue';
     import Types from '@/components/Money/Types.vue';
-    import Notes from '@/components/Money/Notes.vue';
+    import FormItem from '@/components/Money/Formltem.vue';
     import Tags from '@/components/Money/Tags.vue';
     import {Component, Watch} from 'vue-property-decorator';
     import recordListModel from '@/models/recordListModel';
@@ -23,7 +26,7 @@
     const tagList = tagListModel.fetch();
 
     @Component({
-        components: {Tags, Notes, Types, NumberPad}
+        components: {Tags, FormItem, Types, NumberPad}
     })
 
     export default class Money extends Vue {
@@ -65,5 +68,8 @@
     .layout-content {
         display: flex;
         flex-direction: column-reverse;
+    }
+    .notes{
+        padding: 12px 0;
     }
 </style>
