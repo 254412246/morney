f08b267cf990357e6b991fd9e7b50e0fa6f5e29e
Administrator <254412246@qq.com>
2020-04-24 22:48:53 +0800

    消除 Tags 的警告，目前还存在 BUG

--- src/components/Money/Formltem.vue
+++ src/components/Money/Formltem.vue
@@ -4,6 +4,7 @@
             <span class="name">{{this.fieldName}}</span>
             <input type="text"
                    v-model="value"
+                   @input="onValueChanged($event.target.value)"
                    :placeholder='this.placeholder'
             >
         </label>
@@ -17,7 +18,8 @@
     @Component
 
     export default class FormItem extends Vue {
-        value = '';
+        @Prop({default: ''}) readonly value!: string;
+
         @Prop({required: true}) fieldName!: string;
         @Prop() placeholder?: string;
 
