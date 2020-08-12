58de57fc6c7ad005d6749f94e50c38c6d5f4a2f7
Administrator <254412246@qq.com>
2020-08-12 22:44:59 +0800

    重构代码

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,7 +1,10 @@
 <template>
     <Layout>
         <Tabs class-prefix="type" :data-source="recordTypeList" :value.sync="type"/>
-        <Chart :options="x"/>
+        <div class="chart-wrapper" ref="chartWrapper">
+            <Chart class="chart" :options="x"/>
+        </div>
+
         <ol v-if="(groupedList.length>0)">
             <li v-for="(group,index) in groupedList" :key="index">
                 <h3 class="title">{{beautify(group.title)}} <span>￥{{group.total}}</span></h3>
@@ -21,7 +24,122 @@
         </div>
     </Layout>
 </template>
+<script lang="ts">
+    import Vue from 'vue';
+    import {Component} from 'vue-property-decorator';
+    import Tabs from '@/components/Tabs.vue';
+    import recordTypeList from '@/constants/recordTypeList';
+    import dayjs from 'dayjs';
+    import clone from '@/lib/clone';
+    import Chart from '@/components/Chart.vue';
+    import 'echarts/lib/chart/line'
 
+
+
+    @Component({
+        components: {Tabs, Chart},
+    })
+    export default class Statistics extends Vue {
+        tagString(tags: Tag[]) {
+            return tags.length === 0 ? '无' : tags.map(t=>t.name).join('，');
+        }
+        mounted() {
+            const div = (this.$refs.chartWrapper as HTMLDivElement);
+            console.log(div.scrollWidth);
+            div.scrollLeft = div.scrollWidth;
+        }
+        get recordList() {
+            return (this.$store.state as RootState).recordList;
+        }
+
+        get groupedList() {
+            const {recordList} = this;
+
+
+            const newList = clone(recordList)
+                .filter(r => r.type === this.type)
+                .sort((a, b) => dayjs(b.createdAt).valueOf() - dayjs(a.createdAt).valueOf());
+            if (newList.length === 0) {return [];}
+            type Result = { title: string; total?: number; items: RecordItem[] }[]
+            const result: Result = [{title: dayjs(newList[0].createdAt).format('YYYY-MM-DD'), items: [newList[0]]}];
+            for (let i = 1; i < newList.length; i++) {
+                const current = newList[i];
+                const last = result[result.length - 1];
+                if (dayjs(last.title).isSame(dayjs(current.createdAt), 'day')) {
+                    last.items.push(current);
+                } else {
+                    result.push({title: dayjs(current.createdAt).format('YYYY-MM-DD'), items: [current]});
+                }
+            }
+            result.map(group => {
+                group.total = group.items.reduce((sum, item) => {
+                    return sum + item.amount;
+                }, 0);//初始值是0,结果是sum，每一项是item
+            });
+            return result;
+
+        }
+        get x() {
+            return {
+                grid:{
+                    left:0,
+                    right:0
+                },
+                xAxis: {
+                    type: 'category',
+                    data: [
+                        '1', '2', '3', '4', '5', '6', '7', '8', '9', '10',
+                        '11', '12', '13', '14', '15'
+                    ],
+                    axisTick: {alignWithLabel: true},
+                    axisLine: {lineStyle: {color: '#666'}}
+                },
+                yAxis: {
+                    type: 'value',
+                    show: false
+                },
+                series: [{
+                    symbol: 'circle',
+                    symbolSize: 12,
+                    itemStyle: {borderWidth: 1, color: '#666', borderColor: '#666'},
+                    // lineStyle: {width: 10},
+                    data: [
+                        1, 2, 3, 4, 5, 6, 7,
+                        8, 9, 10, 11, 12,13,14,15
+                    ],
+                    type: 'line'
+                }],
+                tooltip: {
+                    show: true, triggerOn: 'click',
+                    position: 'top',
+                    formatter: '{c}'
+                }
+            };
+        }
+        beautify(string: string) {
+            const day = dayjs(string);
+            const now = dayjs();
+            if (day.isSame(now, 'day')) {
+                return '今天';
+            } else if (day.isSame(now.subtract(1, 'day'), 'day')) {
+                return '昨天';
+            } else if (day.isSame(now.subtract(2, 'day'), 'day')) {
+                return '前天';
+            } else if (day.isSame(now, 'year')) {
+                return day.format('M月D日');
+            } else {
+                return day.format('YYYY年M月D日');
+            }
+        }
+
+        beforeCreate() {
+            this.$store.commit('fetchRecords');
+        }
+
+        type = '-';
+        recordTypeList = recordTypeList;
+    }
+</script>
 <style scoped lang="scss">
     %item {
         padding: 8px 16px;
@@ -63,105 +181,18 @@
     }
     .echarts {
         max-width: 100%;
-        height: 400px;
+        height: 200px;
+    }
+    .chart {
+        width: 430%;
+        &-wrapper {
+            overflow: auto;
+            &::-webkit-scrollbar {
+                display: none;
+            }
+        }
     }
 </style>
 
-<script lang="ts">
-    import Vue from 'vue';
-    import {Component} from 'vue-property-decorator';
-    import Tabs from '@/components/Tabs.vue';
-    import recordTypeList from '@/constants/recordTypeList';
-    import dayjs from 'dayjs';
-    import clone from '@/lib/clone';
-    import Chart from '@/components/Chart.vue';
-    import 'echarts/lib/chart/line'
 
-
-
-    @Component({
-        components: {Tabs, Chart},
-    })
-    export default class Statistics extends Vue {
-        tagString(tags: Tag[]) {
-            return tags.length === 0 ? '无' : tags.map(t=>t.name).join('，');
-        }
-
-        get recordList() {
-            return (this.$store.state as RootState).recordList;
-        }
-
-        get groupedList() {
-            const {recordList} = this;
-
-
-            const newList = clone(recordList)
-                .filter(r => r.type === this.type)
-                .sort((a, b) => dayjs(b.createdAt).valueOf() - dayjs(a.createdAt).valueOf());
-            if (newList.length === 0) {return [];}
-            type Result = { title: string; total?: number; items: RecordItem[] }[]
-            const result: Result = [{title: dayjs(newList[0].createdAt).format('YYYY-MM-DD'), items: [newList[0]]}];
-            for (let i = 1; i < newList.length; i++) {
-                const current = newList[i];
-                const last = result[result.length - 1];
-                if (dayjs(last.title).isSame(dayjs(current.createdAt), 'day')) {
-                    last.items.push(current);
-                } else {
-                    result.push({title: dayjs(current.createdAt).format('YYYY-MM-DD'), items: [current]});
-                }
-            }
-            result.map(group => {
-                group.total = group.items.reduce((sum, item) => {
-                    return sum + item.amount;
-                }, 0);//初始值是0,结果是sum，每一项是item
-            });
-            return result;
-
-        }
-        get x() {
-            return {
-                xAxis: {
-                    type: 'category',
-                    data: [
-                        '1', '2', '3', '4', '5', '6', '7', '8', '9', '10',
-                        '11', '12', '13', '14', '15'
-                    ]
-                },
-                yAxis: {
-                    type: 'value'
-                },
-                series: [{
-                    data: [
-                        1, 2, 3, 4, 5, 6, 7,
-                        8, 9, 10, 11, 12,13,14,15
-                    ],
-                    type: 'line'
-                }],
-                tooltip: {show: true}
-            };
-        }
-        beautify(string: string) {
-            const day = dayjs(string);
-            const now = dayjs();
-            if (day.isSame(now, 'day')) {
-                return '今天';
-            } else if (day.isSame(now.subtract(1, 'day'), 'day')) {
-                return '昨天';
-            } else if (day.isSame(now.subtract(2, 'day'), 'day')) {
-                return '前天';
-            } else if (day.isSame(now, 'year')) {
-                return day.format('M月D日');
-            } else {
-                return day.format('YYYY年M月D日');
-            }
-        }
-
-        beforeCreate() {
-            this.$store.commit('fetchRecords');
-        }
-
-        type = '-';
-        recordTypeList = recordTypeList;
-    }
-</script>
 
